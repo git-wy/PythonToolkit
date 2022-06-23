6dc9d01660e7b992acb53896b24edeea925cc4ce
W.Y <861798495@qq.com>
2022-06-23 16:41:12 +0800

    update ImageCrop

--- ImageCrop/image_crop.py
+++ ImageCrop/image_crop.py
@@ -1,33 +1,43 @@
 # -*- coding: utf-8 -*-
-# Author: W.Y.
-# Date: 2022/6/2
+# Author: Ying Wang
 
 from PIL import Image
 import os
-import img2pdf
+
+
+def image_crop(input_dir, output_dir, box_left, box_up, box_right, box_down):
+    """
+    crop all images in a directory
+
+    :param input_dir: directory saving images
+    :param output_dir: directory saving images after cropping
+    :param box_left: the left pixel
+    :param box_up: the upper pixel
+    :param box_right: the right pixel
+    :param box_down: the lower pixel
+    :return: cropped images saved in output_dir
+    """
+
+    for image_file in os.listdir(input_dir):
+
+        image_input_fullname = input_dir + '/' + image_file
+        img = Image.open(image_input_fullname)
+        box = (box_left, box_up, box_left + box_right, box_up + box_down)
+        image_cropped = img.crop(box)
+        image_output_fullname = output_dir + '/' + image_file
+        image_cropped.save(image_output_fullname)
+
+        print('Successfully cropped {}.'.format(image_file))
 
 
 if __name__ == '__main__':
 
-    input_path = r'E:\Users\ying wang\Downloads\iCloud 照片'
-    output_path = r'E:\Users\ying wang\Desktop\新建文件夹'
+    image_input = r'path saving images'
+    image_output = r'path saving images after cropping'
 
-    images = os.listdir(input_path)
-    BOX_LEFT, BOX_UP, BOX_RIGHT, BOX_DOWN = 170, 130, 1890, 1420
+    # image_input = r'E:\ImageCrop\image_input'
+    # image_output = r'E:\ImageCrop\image_output'
 
-    for each_image in images:
+    images = os.listdir(image_input)
+    image_crop(image_input, image_output, 0, 0, 1800, 1400)
 
-        image_input_fullname = input_path + '/' + each_image
-        img = Image.open(image_input_fullname)
-        box = (BOX_LEFT, BOX_UP, BOX_RIGHT + BOX_LEFT, BOX_DOWN + BOX_UP)
-        image_crop = img.crop(box)
-        image_output_fullname = output_path + '/' + each_image
-        image_crop.save(image_output_fullname)
-
-        print('{} done.'.format(each_image))
-
-    pdf_name = output_path + '/' + 'all.pdf'
-
-    with open(pdf_name, "wb") as f:
-        f.write(img2pdf.convert(
-            [os.path.join(output_path, file) for file in images]))
